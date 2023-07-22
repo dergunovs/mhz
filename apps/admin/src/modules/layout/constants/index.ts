4ad028626352b4bd508c2e39e3fbcb1a01004f6b
Aleksandr Dergunov <dergunovs@mail.ru>
2023-07-22 18:29:28 +0300

    feat: nav icons

--- apps/admin/src/modules/layout/constants/index.ts
+++ apps/admin/src/modules/layout/constants/index.ts
@@ -9,15 +9,22 @@ import { URL_CUSTOMER } from '@/customer/constants';
 
 import { INavItem } from '@/layout/interface';
 
-import IconNavProducts from '@/layout/icons/nav-products.svg?component';
+import IconNavMain from '@/layout/icons/nav-main.svg?component';
+import IconNavProduct from '@/layout/icons/nav-product.svg?component';
+import IconNavCategory from '@/layout/icons/nav-category.svg?component';
+import IconNavManufacturer from '@/layout/icons/nav-manufacturer.svg?component';
+import IconNavOrder from '@/layout/icons/nav-order.svg?component';
+import IconNavShipment from '@/layout/icons/nav-shipment.svg?component';
+import IconNavManager from '@/layout/icons/nav-manager.svg?component';
+import IconNavCustomer from '@/layout/icons/nav-customer.svg?component';
 
 export const NAV_ITEMS: INavItem[] = [
-  { _id: 1, url: URL_MAIN, title: 'Главная', icon: IconNavProducts },
-  { _id: 2, url: URL_MANAGER, title: 'Менеджеры', icon: IconNavProducts },
-  { _id: 3, url: URL_SHIPMENT, title: 'Доставка', icon: IconNavProducts },
-  { _id: 4, url: URL_ORDER, title: 'Заказы', icon: IconNavProducts },
-  { _id: 5, url: URL_CATEGORY, title: 'Категории', icon: IconNavProducts },
-  { _id: 6, url: URL_PRODUCT, title: 'Товары', icon: IconNavProducts },
-  { _id: 7, url: URL_MANUFACTURER, title: 'Производители', icon: IconNavProducts },
-  { _id: 8, url: URL_CUSTOMER, title: 'Покупатели', icon: IconNavProducts },
+  { _id: 1, url: URL_MAIN, title: 'Главная', icon: IconNavMain },
+  { _id: 2, url: URL_PRODUCT, title: 'Товары', icon: IconNavProduct },
+  { _id: 3, url: URL_CATEGORY, title: 'Категории', icon: IconNavCategory },
+  { _id: 4, url: URL_MANUFACTURER, title: 'Производители', icon: IconNavManufacturer },
+  { _id: 5, url: URL_ORDER, title: 'Заказы', icon: IconNavOrder },
+  { _id: 6, url: URL_SHIPMENT, title: 'Доставка', icon: IconNavShipment },
+  { _id: 7, url: URL_MANAGER, title: 'Менеджеры', icon: IconNavManager },
+  { _id: 8, url: URL_CUSTOMER, title: 'Покупатели', icon: IconNavCustomer },
 ];
