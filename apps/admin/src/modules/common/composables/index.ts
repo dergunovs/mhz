9467c8f5c1feae62000e74dea39bc3a9d6c202ba
Sergey Krupkin <rekrytkw@gmail.com>
2023-07-23 16:33:54 +0300

    fix (wip): isolate queryClient

--- apps/admin/src/modules/common/composables/index.ts
+++ apps/admin/src/modules/common/composables/index.ts
@@ -1,10 +1,10 @@
 import { useRoute } from 'vue-router';
 import { useQueryClient } from '@tanstack/vue-query';
 
-export function useInvalidate(queryKey: string) {
-  const route = useRoute();
-  const queryClient = useQueryClient();
+const queryClient = useQueryClient(); // стоит проверить, вызвав invalidate
 
+export function useInvalidate(queryKey: string) {
+  const route = useRoute(); 
   function invalidate() {
     if (route.query.create === '1') {
       queryClient.invalidateQueries({ queryKey: [queryKey], exact: true });
