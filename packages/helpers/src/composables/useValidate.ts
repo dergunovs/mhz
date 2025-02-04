20a8f085d33af621a006462cfe5cca1320d7f1f6
Aleksandr Dergunov <dergunovs@mail.ru>
2025-02-04 11:41:25 +0300

    feat: min, max and letters validation rules

--- packages/helpers/src/composables/useValidate.ts
+++ packages/helpers/src/composables/useValidate.ts
@@ -30,9 +30,36 @@ export function useValidator<T>(formData: Ref<T>, rules: ComputedRef<object>) {
 }
 
 export function required(lang?: 'ru' | 'en') {
-  return { required: true, message: lang === 'ru' ? 'Это поле обязательное' : 'This field is required' };
+  return {
+    required: true,
+    whitespace: true,
+    message: lang === 'ru' ? 'Это поле обязательное' : 'This field is required',
+  };
 }
 
 export function email(lang?: 'ru' | 'en') {
   return { type: 'email', message: lang === 'ru' ? 'Введите корретную почту' : 'This is not correct email' };
+}
+
+export function letters(lang?: 'ru' | 'en') {
+  return {
+    validator: (rule: object, value: string) =>
+      /^[a-zA-zа-яА-ЯёЁ-]+([\s][a-zA-Zа-яА-ЯёЁ-]+)*$/.test(value) || !value.length,
+    message: lang === 'ru' ? 'Допустимы только буквы' : 'Only letters',
+    type: 'string',
+  };
+}
+
+export function min(value: number, lang?: 'ru' | 'en') {
+  return {
+    min: value,
+    message: lang === 'ru' ? `Минимальное количество символов: ${value}` : `Minimum symbols: ${value}`,
+  };
+}
+
+export function max(value: number, lang?: 'ru' | 'en') {
+  return {
+    min: value,
+    message: lang === 'ru' ? `Максимальное количество символов: ${value}` : `Maximum symbols: ${value}`,
+  };
 }
