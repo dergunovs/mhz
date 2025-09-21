698015476d7df420a38a612a2285aaaf07fd61b6
Aleksandr Dergunov <dergunovs@mail.ru>
2025-09-21 11:51:32 +0300

    chore: update deps

--- apps/back/src/plugins/rate.ts
+++ apps/back/src/plugins/rate.ts
@@ -2,5 +2,5 @@ import fp from 'fastify-plugin';
 import rateLimit from '@fastify/rate-limit';
 
 export default fp(async function (fastify) {
-  fastify.register(rateLimit, { max: 500, timeWindow: 20000 });
+  fastify.register(rateLimit, { max: 100, timeWindow: 20000 });
 });
